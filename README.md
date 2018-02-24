0506148334ef8066fc371e7b14b8d24864dc1a3f
Zack Scholl <zack.scholl@gmail.com>
2018-02-24 09:42:09 -0800

    Add utils

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# utils
-Useful Golang utils
+<p align="center">
+<strong>utils</strong>
+<br>
+<img src="https://img.shields.io/badge/coverage-50%25-yellow.svg?style=flat-square" alt="Code Coverage">
+<a href="https://godoc.org/github.com/schollz/utils"><img src="https://img.shields.io/badge/godoc-reference-blue.svg?style=flat-square" alt="Code Coverage"></a>
+</p>
+
+<center>This is a bunch of useful utilities I often use in Golang programs</center>
+
+
+# License
+
+MIT 
---
+++ hash.go
@@ -0,0 +1,40 @@
+package utils
+
+import (
+	"bufio"
+	"crypto/md5"
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"os"
+	"regexp"
+)
+
+var convertWindowsLineFeed = regexp.MustCompile(`\r?\n`)
+
+// Filemd5Sum returns the md5 sum of a file and produces the same
+// hash for both Windows and Unix systems.
+func Filemd5Sum(pathToFile string) (result string, err error) {
+	file, err := os.Open(pathToFile)
+	if err != nil {
+		return
+	}
+	defer file.Close()
+	hash := md5.New()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := convertWindowsLineFeed.ReplaceAll(scanner.Bytes(), []byte("\n"))
+		hash.Write(line)
+	}
+	result = hex.EncodeToString(hash.Sum(nil))
+	return
+}
+
+func Hash(data string) string {
+	return HashBytes([]byte(data))
+}
+
+func HashBytes(data []byte) string {
+	sum := sha256.Sum256(data)
+	return fmt.Sprintf("%x", sum)
+}
